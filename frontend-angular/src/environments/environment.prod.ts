9ede732e5ad8b960c1e10d6621e5ad926d0271c3
CodeWithElias <eliaspumaclaure48@gmail.com>
2026-04-27 02:39:45 -0400

    correccion de api websocker

--- frontend-angular/src/environments/environment.prod.ts
+++ frontend-angular/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
     //apiUrl: 'http://3.92.84.71:8080/api', 
     //iaApiUrl: 'http://3.92.84.71:8000',
     apiUrl: 'https://d2oa5maju2bq22.cloudfront.net/api', 
-    iaApiUrl: 'https://d2oa5maju2bq22.cloudfront.net/api',
+    iaApiUrl: 'https://d2oa5maju2bq22.cloudfront.net/ia',
     cloudinary: {
         cloudName: 'dsu8dll2n',
         uploadPreset: 'tramites_workflow'
--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -2,8 +2,10 @@ export const environment = {
     production: false,
     //apiUrl: 'http://localhost:8080/api', // Tu backend local
     //iaApiUrl: 'http://localhost:8000/api',
-    apiUrl: 'http://3.92.84.71:8080/api', // Tu backend local
-    iaApiUrl: 'http://3.92.84.71:8000/ia',
+    //apiUrl: 'http://3.92.84.71:8080/api', // Tu backend local
+    //iaApiUrl: 'http://3.92.84.71:8000/ia',
+    apiUrl: 'https://d2oa5maju2bq22.cloudfront.net/api', 
+    iaApiUrl: 'https://d2oa5maju2bq22.cloudfront.net/ia',
     cloudinary: {
         cloudName: 'dsu8dll2n',
         uploadPreset: 'tramites_workflow'
