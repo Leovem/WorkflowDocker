7ede2b7f424e1cb761ceee292470f501de942423
CodeWithElias <eliaspumaclaure48@gmail.com>
2026-04-27 00:10:16 -0400

    correcciones del cors

---
+++ frontend-angular/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://3.92.84.71:8080/api', 
+    iaApiUrl: 'http://3.92.84.71:8000',
+    cloudinary: {
+        cloudName: 'dsu8dll2n',
+        uploadPreset: 'tramites_workflow'
+    }
+};
